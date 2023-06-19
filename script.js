4c4672419a99218793cb423efd94b46c592822ad
diptobiswas3d <diptobiswasgpt4@gmail.com>
2023-06-19 16:49:20 +0530

    feat - added, if two squares are close they're connected by a line

---
+++ script.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = 400
+canvas.height = 400
+
+class Particle {
+    constructor(shape = "square") {
+        this.x = Math.random() * canvas.width
+        this.y = Math.random() * canvas.height
+        this.speedX = Math.random() * 6 - 3
+        this.speedY = Math.random() * 6 - 3
+        this.edge = 10
+        this.shape = shape
+    }
+    // Todo - Draw different particles depending on input, square, circle, etc.
+    // Drawing the particle
+    draw() {
+        ctx.fillRect(this.x, this.y, this.edge, this.edge)
+    }
+    // Updating the particle
+    update() {
+        if (this.y < 0 || this.y + this.edge > canvas.height) {
+            this.speedY = -this.speedY
+        }
+        if (this.x < 0 || this.x + this.edge > canvas.width) {
+            this.speedX = -this.speedX
+        }
+        this.x += this.speedX
+        this.y += this.speedY
+        this.draw()
+    }
+}
+
+let particleArray = []
+
+// Todo - Particle count should be a user input
+let particleCount = 75
+
+for (let i = 0; i < particleCount; i++) {
+    particleArray.push(new Particle())
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < particleCount; i++) {
+        particleArray[i].update()
+    }
+    for (let i = 0; i < particleCount; i++) {
+        for (let j = 0; j < particleCount; j++) {
+            if (distance_between_two_points(particleArray[i].x, particleArray[i].y, particleArray[j].x, particleArray[j].y) < 100) {
+                ctx.beginPath()
+                ctx.moveTo(particleArray[i].x, particleArray[i].y)
+                ctx.lineTo(particleArray[j].x, particleArray[j].y)
+                ctx.stroke()
+            }
+        }
+    }
+    requestAnimationFrame(animate)
+}
+
+
+animate()
+
+function distance_between_two_points(x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    return Math.sqrt(dx * dx + dy * dy);
+}
